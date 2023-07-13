ac6c566ef8ae59c6c7dd8e360834b1b4ce57b2cd
Alejandra <amzita@scl.edu.gt>
2023-07-13 13:46:06 -0600

    Evaluacion

--- js/main.js
+++ js/main.js
@@ -1,38 +1,42 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => response.json())
+.then(users => {
+  const userDetailsContainer = document.getElementById('user-details');
 
-  // Obtener los datos de los usuarios desde la API
-  fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
-    .then(response => response.json())
-    .then(users => {
-      const userDetailsContainer = document.getElementById('user-details');
+  users.forEach(user => {
+    const userCard = document.createElement('div');
+    userCard.classList.add('card');
 
-      // Iterar sobre los usuarios y crear los componentes individuales
-      users.forEach(user => {
-        const userComponent = document.createElement('div');
-        userComponent.classList.add('user-component');
+    const profileImage = document.createElement('img');
+    profileImage.classList.add('profile-image');
+    profileImage.src = 'https://i.pinimg.com/564x/1e/e3/ae/1ee3aeb81e19166fa2f59a99c932649f.jpg';
+    profileImage.alt = 'Profile Image';
 
-        const username = document.createElement('p');
-        username.classList.add('name');
-        username.textContent = `Username: ${user.name}`;
+    const username = document.createElement('p');
+    username.classList.add('name');
+    username.textContent = ` ${user.name}`;
 
-        const email = document.createElement('p');
-        email.textContent = `Email: ${user.email}`;
+    const email = document.createElement('p');
+    email.textContent = `Email: ${user.email}`;
 
-        const phone = document.createElement('p');
-        phone.textContent = `Phone: ${user.phone}`;
+    const phone = document.createElement('p');
+    phone.textContent = `Phone: ${user.phone}`;
 
-        const company = document.createElement('p');
-        company.textContent = `Company: ${user.company.name}`;
+    const company = document.createElement('p');
+    company.textContent = `Company: ${user.company.name}`;
 
-        userComponent.appendChild(username);
-        userComponent.appendChild(email);
-        userComponent.appendChild(phone);
-        userComponent.appendChild(company);
+    userCard.appendChild(profileImage);
+    userCard.appendChild(username);
+    userCard.appendChild(email);
+    userCard.appendChild(phone);
+    userCard.appendChild(company);
 
-        userDetailsContainer.appendChild(userComponent);
-      });
-    })
-    .catch(error => console.error(error));
-  
+    userDetailsContainer.appendChild(userCard);
 
-  
- 
+    console.log('Username:', user.name);
+    console.log('Email:', user.email);
+    console.log('Phone:', user.phone);
+    console.log('Company:', user.company.name);
+  });
+})
+.catch(error => console.error(error));
